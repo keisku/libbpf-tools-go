4c519541a77d7499de55aa567f776160d159ebcf
keisku <keisuke.umegaki.630@gmail.com>
2023-01-18 05:50:48 +0000

    add bpf programs to collect execsnoop events

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
+CLANG ?= clang
+CFLAGS := -O2 -g -Wall -Werror $(CFLAGS)
+
+generate: export BPF_CLANG := $(CLANG)
+generate: export BPF_CFLAGS := $(CFLAGS)
 generate:
 	bpftool btf dump file /sys/kernel/btf/vmlinux format c > ./headers/vmlinux.h
+	go generate ./...
--- cmd/execsnoop/main.go
+++ cmd/execsnoop/main.go
@@ -2,4 +2,7 @@ package main
 
 import _ "github.com/cilium/ebpf"
 
+// $BPF_CLANG and $BPF_CFLAGS are set by the Makefile.
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS -target amd64 bpf execsnoop.bpf.c -- -I../../headers
+
 func main(){}
